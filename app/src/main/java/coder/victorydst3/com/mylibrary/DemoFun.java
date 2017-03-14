b4f8498ad9dda5000df1ad7653a01c9bf32377fe
vinh.huynh <vinh.huynh@asiantech.vn>
2017-03-14 09:52:27 +0700

    init

---
+++ app/src/main/java/coder/victorydst3/com/mylibrary/DemoFun.java
@@ -0,0 +1,16 @@
+package coder.victorydst3.com.mylibrary;
+
+/**
+ * Copyright © 2016 AsianTech inc.
+ * Created by VinhHLB on 3/14/17.
+ */
+
+public final class DemoFun {
+    private DemoFun() {
+    }
+
+    public static String getString() {
+        return "String from sub-module";
+    }
+
+}
